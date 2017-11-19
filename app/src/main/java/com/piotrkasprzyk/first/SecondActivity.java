b963a7cc057cc04995c91de8132e02dae8de137b
kaspiotr <kaspiotr@gmail.com>
2017-11-19 17:09:24 +0100

    Remove debug button

--- app/src/main/java/com/piotrkasprzyk/first/SecondActivity.java
+++ app/src/main/java/com/piotrkasprzyk/first/SecondActivity.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 
 public class SecondActivity extends AppCompatActivity {
     @Override
@@ -15,7 +14,5 @@ public class SecondActivity extends AppCompatActivity {
         setSupportActionBar(myToolbar);
     }
 
-    public void closeSecondActivity(View view) {
-        onBackPressed();
-    }
+
 }
