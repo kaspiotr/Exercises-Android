ecd5601d9f842ab7619639986c4ed8f8a8535532
kaspiotr <kaspiotr@gmail.com>
2017-11-19 21:58:21 +0100

    Add support action bar with the app action enabled

--- app/src/main/java/com/piotrkasprzyk/first/SecondActivity.java
+++ app/src/main/java/com/piotrkasprzyk/first/SecondActivity.java
@@ -2,6 +2,7 @@ package com.piotrkasprzyk.first;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
@@ -10,8 +11,13 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.second_activity);
-        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        Toolbar myToolbar = findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
     }
 
 
